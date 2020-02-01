a13d80fcc39ba22a3b3ad8c097c363ab6a5a0ba1
Murat Slavich <slavichmo@gmail.com>
2020-02-02 01:38:34 +0700

    add builder

--- src/creational/Builder/BankAccount.kt
+++ src/creational/Builder/BankAccount.kt
@@ -1,4 +1,65 @@
 package creational.Builder
 
-class BankAccount(val accountNumber: Long) {
+class BankAccount(builder: BankAccount.Builder) {
+    private val accountNumber: Double?
+    private val owner: String?
+    private val branch: String?
+    private val balance: Double?
+    private val interestRate: Double?
+
+    init {
+        accountNumber = builder.accountNumber
+        owner = builder.owner
+        branch = builder.branch
+        balance = builder.balance
+        interestRate = builder.interestRate
+    }
+
+    class Builder {
+        var accountNumber: Double? = null
+        var owner: String? = null
+        var branch: String? = null
+        var balance: Double? = null
+        var interestRate: Double? = null
+
+        fun accountNumber(accountNumber: Double) = apply { this.accountNumber = accountNumber }
+        fun owner(owner: String) = apply { this.owner = owner }
+        fun branch(branch: String) = apply { this.branch = branch }
+        fun balance(balance: Double) = apply { this.balance = balance }
+        fun interestRate(interestRate: Double) = apply { this.interestRate = interestRate }
+        fun build() = BankAccount(this)
+    }
+
+    override fun toString(): String {
+        return "BankAccount(accountNumber=$accountNumber, owner=$owner, branch=$branch, balance=$balance, interestRate=$interestRate)"
+    }
+
+
+}
+
+class KotlinBankAccount(
+    private val accountNumber: Double?,
+    private val owner: String?,
+    private val branch: String?,
+    private val balance: Double?,
+    private val interestRate: Double?
+) {
+    data class Builder(
+        private var accountNumber: Double? = null,
+        private var owner: String? = null,
+        private var branch: String? = null,
+        private var balance: Double? = null,
+        private var interestRate: Double? = null
+    ) {
+        fun accountNumber(accountNumber: Double) = apply { this.accountNumber = accountNumber }
+        fun owner(owner: String) = apply { this.owner = owner }
+        fun branch(branch: String) = apply { this.branch = branch }
+        fun balance(balance: Double) = apply { this.balance = balance }
+        fun interestRate(interestRate: Double) = apply { this.interestRate = interestRate }
+        fun build() = KotlinBankAccount(accountNumber, owner, branch, balance, interestRate)
+    }
+
+    override fun toString(): String {
+        return "KotlinBankAccount(accountNumber=$accountNumber, owner=$owner, branch=$branch, balance=$balance, interestRate=$interestRate)"
+    }
 }
--- src/creational/Builder/Main.kt
+++ src/creational/Builder/Main.kt
@@ -1,5 +1,16 @@
 package creational.Builder
 
 fun main() {
-    val bankAccount = BankAccount(12)
+    var bankAccount = BankAccount.Builder()
+        .accountNumber(12.0)
+        .balance(200.0)
+        .branch("develop")
+        .build()
+
+    val kotlinBankAccount = KotlinBankAccount.Builder()
+        .accountNumber(1222.0)
+        .build()
+
+    println(bankAccount)
+    println(kotlinBankAccount)
 }
