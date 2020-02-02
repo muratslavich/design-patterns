d15cd1aa2651473a3d8dcc6e3659c4b0573cfa0d
Murat Slavich <slavichmo@gmail.com>
2020-02-02 19:50:18 +0300

    add Adapter

---
+++ src/structural/Adapter/Adapter.kt
@@ -0,0 +1,25 @@
+package structural.Adapter
+
+import java.math.*
+
+interface Temperature {
+    var temperature: Double
+}
+
+open class CelsiusTemperature(override var temperature: Double): Temperature
+class FahrenheitTemperature(override var temperature: Double): Temperature
+
+class FahrenheitAdapter(private val celsiusTemperature: CelsiusTemperature) {
+    fun convertToFahrenheitTemperature(): FahrenheitTemperature = FahrenheitTemperature(
+        ((BigDecimal.valueOf(celsiusTemperature.temperature).setScale(2) * BigDecimal(9) / BigDecimal(5)) + BigDecimal(32))
+        .toDouble()
+    )
+}
+
+fun main() {
+    val temperature = CelsiusTemperature(45.0)
+
+    val fahrenheitTemperature = FahrenheitAdapter(temperature).convertToFahrenheitTemperature()
+
+    print(fahrenheitTemperature.temperature)
+}
