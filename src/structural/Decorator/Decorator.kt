5c03106a5582863053c2327c0f184d3b11739d51
Murat Slavich <slavichmo@gmail.com>
2020-02-04 19:13:07 +0300

    add Decorator

---
+++ src/structural/Decorator/Decorator.kt
@@ -0,0 +1,35 @@
+package structural.Decorator
+
+interface CoffeeMachine {
+    fun makeSmallCoffee()
+    fun makeLargeCoffee()
+}
+
+class NormalCoffeeMachine : CoffeeMachine {
+    override fun makeSmallCoffee() = println("Normal small coffee")
+    override fun makeLargeCoffee() = println("Normal large coffee")
+}
+
+class EnhancedCoffeeMachine(private val coffeeMachine: CoffeeMachine) : CoffeeMachine by coffeeMachine {
+
+    override fun makeSmallCoffee() {
+        println("Enhanced small coffee")
+    }
+
+    fun makeCoffeeWithMilk() {
+        makeSmallCoffee()
+        addMilk()
+        println("Enhanced small coffee with milk")
+    }
+
+    private fun addMilk() {}
+}
+
+fun main() {
+    val normalCoffeeMachine = NormalCoffeeMachine()
+    val enhancedCoffeeMachine = EnhancedCoffeeMachine(normalCoffeeMachine)
+
+    enhancedCoffeeMachine.makeSmallCoffee()
+    enhancedCoffeeMachine.makeLargeCoffee()
+    enhancedCoffeeMachine.makeCoffeeWithMilk()
+}
