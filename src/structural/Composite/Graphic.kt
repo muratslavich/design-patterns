fd02c09a12de714d8cf00dab53daff895cee9895
muratslavich <slavichmo@gmail.com>
2020-10-13 02:47:06 +0300

    fix composite

--- src/structural/Composite/Graphic.kt
+++ src/structural/Composite/Graphic.kt
@@ -1,9 +1,14 @@
+package structural.Composite
 
+// hierarchy
 open class Equipment(open val price: Int, name: String)
+class Processor: Equipment(1070, "structural.Composite.Processor")
+class HardDrive: Equipment(250, "Hard")
+class Memory: Equipment(280, "structural.Composite.Memory")
 
+// composite
 open class Composite(name: String): Equipment(0, name) {
     private val equipments = ArrayList<Equipment>()
-
     override val price: Int
         get() = equipments.map { it.price }.sum()
 
@@ -11,10 +16,6 @@ open class Composite(name: String): Equipment(0, name) {
 }
 
 class PersonalComputer: Composite("PC")
-
-class Processor: Equipment(1070, "Processor")
-class HardDrive: Equipment(250, "Hard")
-class Memory: Equipment(280, "Memory")
 
 
 fun main() {
